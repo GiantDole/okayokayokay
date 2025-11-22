6941a27dde4d2888a7d7f0843d9a1fa365ca6945
eduramme <eduramme@gmail.com>
2025-11-21 22:25:07 -0300

    fix: styles and texts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">okayokayokay</h1>
+    <main className="flex min-h-screen items-start flex-col  p-24">
+      <h1 className="text-4xl font-bold text-white">Dashboard</h1>
     </main>
   );
 }
