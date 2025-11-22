d0a310f216f1df8f38177822561e8c4f2f7f34bd
eduramme <eduramme@gmail.com>
2025-11-21 22:28:00 -0300

    feat: create disputes page

---
+++ frontend/app/disputes/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-start flex-col  p-24">
+      <h1 className="text-4xl font-bold text-white">Disputes</h1>
+    </main>
+  );
+}
