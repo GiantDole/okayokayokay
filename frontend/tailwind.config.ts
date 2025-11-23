3d3911660aecbb82616745de1e939d88aa200d52
eduramme <eduramme@gmail.com>
2025-11-23 05:04:11 -0300

    upgrade highlight color

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,7 +19,7 @@ export default {
         success: "#4ADE80",
         warning: "#FACC15",
         error: "#F87171",
-        highlight: "#EC762E",
+        highlight: "#29A694",
         background: "#09090B",
       },
     },
